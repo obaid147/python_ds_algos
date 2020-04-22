3516c672354458a00eccede0b2c5d75e4833b6c1
obaid147 <obaidfayazwani@gmail.com>
2020-04-22 21:44:04 +0530

    under math package added new file with an iterative method for euclid's algo for to find GCD...

--- math/EuclidsAlgorithm_gcd.py
+++ math/EuclidsAlgorithm_gcd.py
@@ -1,4 +1,4 @@
-def computeGCD(n1, n2):
+def method1(n1, n2):
     while n2:
         n1 = n2
         n2 = n1 % n2
@@ -6,4 +6,4 @@ def computeGCD(n1, n2):
     return n1
 
 
-print("GCD = ", computeGCD(6, 2))
+print("GCD = ", method1(6, 2))
