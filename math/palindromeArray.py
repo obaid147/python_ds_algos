f55feef09015df9c3f50d367f959ef22679c0a69
aamir-f <dropmeataamir@gmail.com>
2020-04-24 18:17:28 +0530

    Arry-palindrome

--- math/palindromeArray.py
+++ math/palindromeArray.py
@@ -1,16 +1,20 @@
-array = [1, 2, 3, 4, 2, 1]
-i = 0
-j = len(array) - 1
-flag = False
-while i < j:
-    if array[i] == array[j]:
-        i = i + 1
-        j = j - 1
-        flag = True
+def method1(array, j, flag, i):
+    while i < j:
+        if array[i] == array[j]:
+            i = i + 1
+            j = j - 1
+            flag = True
+        else:
+            flag = False
+            break
+    if flag:
+        print(array, 'is Palindrome')
     else:
-        flag = False
-        break
-if flag:
-    print(array, 'is Palindrome')
-else:
-    print(array, 'is  not Palindrome')
+        print(array, 'is  not Palindrome')
+
+
+if __name__ == '__main__':
+    arr = [1, 2, 4, 2, 1]
+    size = len(arr) - 1
+    flag1 = True
+    method1(arr, size, flag1, i=0)
