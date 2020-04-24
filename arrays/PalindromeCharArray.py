82d4a8850c842f5b5d94eb9cc3b75e921a2fb740
aamir-f <dropmeataamir@gmail.com>
2020-04-24 20:43:23 +0530

    palindrome CHAR array

--- arrays/PalindromeCharArray.py
+++ arrays/PalindromeCharArray.py
@@ -1,1 +1,22 @@
-arr = ['m', 'a', 'd', 'a', 'm']
+# TC --> O(n) aux-space O(1)
+def method1(array):
+    flag = True
+    i = 0
+    j = len(arr) - 1
+    while i < j:
+        if array[i] != array[j]:
+            flag = False
+            break
+        else:
+            i = i + 1
+            j = j - 1
+            flag = True
+    if flag:
+        print(array, 'is Palindrome')
+    else:
+        print(array, 'is  not Palindrome')
+
+
+if __name__ == '__main__':
+    arr = ['m', 'a', 'd', 'd', 'a', 'm']
+    method1(arr)
