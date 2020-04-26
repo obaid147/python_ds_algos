7346e8111fffbb4d55568d26fe8622f0444da9cb
obaid147 <obaidfayazwani@gmail.com>
2020-04-26 21:46:39 +0530

    Array Alternate

--- arrays/AlternativeOrder.py
+++ arrays/AlternativeOrder.py
@@ -1,18 +1,10 @@
-array1 = [1, 2, 3, 4, 5]
-array2 = [1, 2, 3, 4]
-i = i+2
-for i in range(1, len(array1) + 1):
-    if i & 1:
-        print(i, end=' ')
-print()
-# =====================================
+def method1(arr):
+    for i in range(1, len(arr)+1, 2):
+        if i & 1:
+            print(i, end=' ')
 
 
-# ======================================
-
-nums = [1, 0, 0, 4]
-nums1 = [1, 2, 1]
-for i in range(len(nums)):
-    if nums[i] == 0:
-        nums[i] = 5
-print(nums)
+if __name__ == '__main__':
+    array1 = [1, 2, 3, 4, 5]
+    array2 = [1, 2, 3, 4]
+    method1(array1)
