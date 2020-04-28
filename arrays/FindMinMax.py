921d0faf9cebb428c19a465c6f1c1605ad440903
obaid147 <obaidfayazwani@gmail.com>
2020-04-28 16:06:22 +0530

    Minimum and Maximum Numbers in array

---
+++ arrays/FindMinMax.py
@@ -0,0 +1,38 @@
+def maxMin(arr):
+    # TC--> O(n)
+    gr8 = arr[0]
+    for i in arr:
+        if i > gr8:
+            gr8 = i
+    print('Highest = ', gr8)
+
+    # TC--> O(n)
+    low = arr[0]
+    for i in arr:
+        if i < low:
+            low = i
+    print('Lowest = ', low)
+
+
+def secondMaxMin(arr):
+    # ===2nd Highest/lowest======
+    arr.remove(max(arr))
+    gr8 = arr[0]
+    for i in arr:
+        if i > gr8:
+            gr8 = i
+    print('2nd Highest = ', gr8)
+
+    arr.remove((min(arr)))
+    low = arr[0]
+    print(arr)
+    for i in arr:
+        if i < low:
+            low = i
+    print('2nd Lowest = ', low)
+
+
+if __name__ == '__main__':
+    array = [1, 3, 5, 7, 9, 2]
+    maxMin(array)
+    secondMaxMin(array)
