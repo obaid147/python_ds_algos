b7a54683d899c8cd0473542e4368da1d7f572fc8
obaid147 <obaidfayazwani@gmail.com>
2020-04-28 21:02:46 +0530

    Greatest 2nd, lowest 2nd

--- arrays/FindMinMax.py
+++ arrays/FindMinMax.py
@@ -1,59 +1,34 @@
-def maxMin(arr):
-    # TC--> O(n)
-    gr8 = arr[0]
+def Greatest(arr):
+    greatest = arr[0]
     for i in arr:
-        if i > gr8:
-            gr8 = i
-    print('Highest = ', gr8)
+        if i > greatest:
+            greatest = i
+    print('Greatest = ', greatest)
 
-    # TC--> O(n)
-    low = arr[0]
-    for i in arr:
-        if i < low:
-            low = i
-    print('Lowest = ', low)
-
-
-def secondMaxMin(arr):
-    # ===2nd Highest/lowest======
+    secGreatest = arr[0]
     arr.remove(max(arr))
-    gr8 = arr[0]
     for i in arr:
-        if i > gr8:
-            gr8 = i
-    print('2nd Highest = ', gr8)
+        if i > secGreatest:
+            secGreatest = i
+    print('Second Greatest = ', secGreatest)
+
+
+def Lowest(arr):
+    lowest = arr[0]
+    for i in arr:
+        if i < lowest:
+            lowest = i
+    print('Lowest = ', lowest)
 
     arr.remove((min(arr)))
     low = arr[0]
     for i in arr:
         if i < low:
             low = i
-    print('2nd Lowest = ', low)
-
-
-def thirdMaxMin():
-    # ===3rd Highest/lowest======
-    arr = [100, 500, 900, 760, 800, 300]
-    arr.remove(max(arr))
-    arr.remove(max(arr))
-    gr8 = arr[0]
-    for i in arr:
-        if i > gr8:
-            gr8 = i
-    print('3rd Highest = ', gr8)
-
-    arr.remove((min(arr)))
-    arr.remove((min(arr)))
-    low = arr[0]
-    for i in arr:
-        if i < low:
-            low = i
-    print('3rd Lowest = ', low)
+    print('Second Lowest = ', low)
 
 
 if __name__ == '__main__':
     array = [100, 500, 900, 760, 800, 300]
-    print(array)
-    maxMin(array)
-    secondMaxMin(array)
-    thirdMaxMin()
+    Greatest(array)
+    # Lowest(array)
