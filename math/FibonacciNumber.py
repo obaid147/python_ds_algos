0f4cfbceea459f2cf4591b820493d840b7a0b22c
obaid147 <obaidfayazwani@gmail.com>
2020-04-29 18:44:55 +0530

    fibonacci number

--- math/FibonacciNumber.py
+++ math/FibonacciNumber.py
@@ -3,12 +3,8 @@ index = int(input("Enter Index: "))
 a, b = 0, 1
 count = 0
 
-if index == a:
-    print(a)
-elif index == b:
-    print(b)
-else:
-    while count < index:
-        a, b = b, a + b
-        count += 1
-    print(a)
+while count < index:
+    a, b = b, a + b
+    count += 1
+print(a)
+
