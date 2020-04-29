5f2f5bf034daf3341f12fc9f4f8eef8f5bf0fe12
obaid147 <obaidfayazwani@gmail.com>
2020-04-29 15:56:12 +0530

    fibonacci number

--- math/FibonacciNumber.py
+++ math/FibonacciNumber.py
@@ -1,14 +1,14 @@
 index = int(input("Enter Index: "))
 
-n1, n2 = 0, 1
+a, b = 0, 1
 count = 0
 
-if index == n1:
-    print(n1)
-elif index == n2:
-    print(n2)
+if index == a:
+    print(a)
+elif index == b:
+    print(b)
 else:
     while count < index:
-        n1, n2 = n2, n1+n2
+        a, b = b, a + b
         count += 1
-    print(n1)
+    print(a)
