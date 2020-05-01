b70588a78809c8f5eaa91347d5106bdd0f3c06dd
obaid147 <obaidfayazwani@gmail.com>
2020-05-01 15:26:28 +0530

    minmax

--- arrays/FindMinMax.py
+++ arrays/FindMinMax.py
@@ -20,19 +20,19 @@ def lowest(arr):
 
 def greatest(arr):
     first = -100000000
-    second = -10000000
+    secon = -10000000
     third = -100000000
     for i in range(len(arr)):
         if arr[i] > first:
-            third = second
-            second = first
+            third = secon
+            secon = first
             first = arr[i]
-        elif arr[i] > second and arr[i] != first:
-            second = arr[i]
-        elif arr[i] > third and arr[i] != second:
+        elif arr[i] > secon and arr[i] != first:
+            secon = arr[i]
+        elif arr[i] > third and arr[i] != secon:
             third = arr[i]
 
-    print(first, second, third)
+    print(first, secon, third)
 
 
 array = [10, -6, -6, 12, 3, -20, 14, 8]
