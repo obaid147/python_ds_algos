8a921abdb7b15fd0b4ca088eb6be53dedb33ff51
obaid147 <obaidfayazwani@gmail.com>
2020-05-04 17:57:02 +0530

    Array elments <= given num

--- arrays/ElementsLessOrEqualToNumber.py
+++ arrays/ElementsLessOrEqualToNumber.py
@@ -1,4 +1,4 @@
-def m1():
+def m1(arr):
     count = 0
     for i in range(len(arr)-1):
         if arr[i] <= n:
@@ -6,8 +6,26 @@ def m1():
     return count
 
 
-arr = [1, 2, 2, 2, 3, 4]  # ip 3, op 5
-# arr = [1,2,3,4,5,6,7,8,9]  # ip 9 op 9
+def m2(arr, key):
+    low = 0
+    high = len(arr) - 1
+    count = 0
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == key:
+            return mid + 1
+
+        if arr[mid] <= key:
+            count = mid + 1
+            low = mid + 1
+        else:
+            high = mid - 1
+    return count
+
+
+# arr = [1, 2, 2, 2, 3, 4]  # ip 3, op 5
+array = [1, 2, 3, 4, 5, 6, 7]  # ip 9 op 9
 
 n = int(input("Enter a number: "))
-print(m1())
+print(m2(array, n))
