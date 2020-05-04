5cf3e7237a6edbab457d40c830b5684802051647
obaid147 <obaidfayazwani@gmail.com>
2020-05-04 21:06:30 +0530

    2 raisedPower input

--- math/PowNumber.py
+++ math/PowNumber.py
@@ -1,9 +1,13 @@
-def powerOfNumber(cons, inp):
-    # res = 2**num
-    # print(res)
-    print("2^{} is: {}".format(inp, cons**inp))
+def powerOfNumber(var, inp):
+    if inp == 0:
+        return 1
+    if inp == 1:
+        return var
+    else:
+        return powerOfNumber(var * base, inp - 1)
 
 
-constant = 2
+base = 2
+variable = base
 inputNum = int(input("Enter a number: "))
-powerOfNumber(constant, inputNum)
+print(powerOfNumber(variable, inputNum))
