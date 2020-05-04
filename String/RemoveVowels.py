4aad81f9145cbb173c7274825e892b0948ca44ed
obaid147 <obaidfayazwani@gmail.com>
2020-05-04 20:03:35 +0530

    emove vowels

--- String/RemoveVowels.py
+++ String/RemoveVowels.py
@@ -1,12 +1,12 @@
-def remove_voewls(string):
+def remove_vowels(string):
     vowels = 'aeiouAEIOU'
     for i in string:
         if i in vowels:
-            # string[x] = '' It shows item assignment not possible
+            # string[i] = '' It shows item assignment not possible
             string = string.replace(i, "")
     print(string)
 
 
 while True:
     str1 = input("Enter a String: ")
-    remove_voewls(str1)
+    remove_vowels(str1)
