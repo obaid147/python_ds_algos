3758eecba185c4b787ec8b4e00b7f92ac56410aa
obaid147 <obaidfayazwani@gmail.com>
2020-05-05 18:51:47 +0530

    Tower Signal

---
+++ arrays/TowerSignal.py
@@ -0,0 +1,21 @@
+# towers = [90, 70, 60, 80, 70, 90]
+towers = [12, 100, 40, 60, 120, 90]
+newStack = []
+size = len(towers)
+size = -abs(size)
+print(towers)
+a = 1
+i = -1
+while i >= size:
+    a = 1
+    j = i-1
+    while j >= size:
+        if towers[i] > towers[j]:
+            a = a + 1
+        else:
+            break
+        j -= 1
+    newStack.insert(i, a)
+    i -= 1
+
+print(newStack)
