c271019cf0565f74448041f3d6ddf2856c51d3d2
obaid147 <obaidfayazwani@gmail.com>
2020-05-17 20:04:58 +0530

    printReverse recursive

--- linkedlist/singly/PrintLinkedList.py
+++ linkedlist/singly/PrintLinkedList.py
@@ -18,6 +18,13 @@ class SinglyLinkedList:
                 print(temp.data, end=' ')
                 temp = temp.next
 
+    # Recursive
+    def printReverse(self, head):
+        if head is None:
+            return
+        self.printReverse(head.next)
+        print(head.data)
+
 
 ll = SinglyLinkedList()
 ll.head = Node(1)
@@ -29,4 +36,5 @@ ll.head.next = second
 second.next = third
 third.next = four
 four.next = five
-ll.printList()
+# ll.printList()
+ll.printReverse(ll.head)
---
+++ math/Multiples_3or5.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number: "))
+a = 3
+d = 3
+n = x//a
+sum1 = d*n*(n+1) // 2
+print(sum1)
